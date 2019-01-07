733af6e844eb76721754e18c5271f4b049865ac7
Alex Miner <alex.miner@gmail.com>
2019-01-06 20:43:09 -0800

    update server requests

--- proxy/server.js
+++ proxy/server.js
@@ -18,21 +18,21 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/items/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/items/${ req.params.itemId }`)
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/items/${ req.params.itemId }`)
   .then(items => res.status(200).send(items.data))
   .catch(() => console.log('ERROR IN PROXY SERVER /API/ITEMS/:ITEMID'));
 });
 
 app.get('/api/related/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/related/${ req.params.itemId }`)
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/related/${ req.params.itemId }`)
   .then(relatedItems => res.status(200).send(relatedItems.data))
   .catch(() => console.log('ERROR IN PROXY SERVER /API/RELATED/:ITEMID'));
 });
 
 app.get('/api/frequent/:itemId', (req, res) => {
-  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/frequent/${ req.params.itemId }`)
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com:8888/api/frequent/${ req.params.itemId }`)
   .then(frequentlyTogether => res.status(200).send(frequentlyTogether.data))
-  .catch(() => console.log('ERROR IN PROXY SERVER /API/FREQUENT/:ITEMID'))
+  .catch(() => console.log('ERROR IN PROXY SERVER /API/FREQUENT/:ITEMID'));
 });
 
 app.post('/api/messages', (req, res) => {
